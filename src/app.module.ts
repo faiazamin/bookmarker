3f36a69aaa6a4da95f35daefba9b97b9bacc39ac
faiazamin <faiazamin.24csedu.040@gmail.com>
2024-03-02 17:31:44 +0600

    jwt done

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,15 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
-  imports: [BookmarkModule, AuthModule, UserModule, PrismaModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal:true,
+  }), BookmarkModule, AuthModule, UserModule, PrismaModule,JwtModule.register({})],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService,JwtStrategy],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaPromise } from '@prisma/client/runtime/library';
+import { config } from 'process';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+    constructor(
+        private prismaService: PrismaService,
+        private jwt: JwtService,
+        private config:ConfigService,
+    ){}
     login() {
         return 'signed in'
     }
 
     signup() {
         return 'signed up'
+    }
+
+    async signToken(userId: number, email: string) {
+        const payload = {
+            sub: userId,
+            email,
+        }
+        const access_token = await this.jwt.signAsync(payload, {
+            expiresIn: '30m',
+            secret:this.config.get('JWT_SECRET'),
+        }
+        )
+        return {
+            access_token:access_token
+        }
     }
 }
